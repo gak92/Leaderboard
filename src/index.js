7a77c5fadc76b84940fd3aece71e0b1d3f6f1eee
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-20 18:03:10 +0500

    Add functionality for refresh button to get score list using API and display name and score

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const gameID = 'geNkiCY06ZXBLWm1WIb4';
+const requestedURL = `${baseURL}/games/${gameID}/scores`;
+const btnRefresh = document.querySelector('.btn-refresh');
+const scoreList = document.querySelector('.score-list');
+
+const renderScores = (items) => {
+  let listItem = '';
+  items.forEach((item) => {
+    listItem += `<li>${item.user}: ${item.score}</li>`;
+  });
+  scoreList.innerHTML = listItem;
+};
+
+const getScores = async () => {
+  await fetch(requestedURL)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json.result);
+      renderScores(json.result);
+    });
+};
+
+btnRefresh.addEventListener('click', () => {
+  getScores();
+});
+
+getScores();
